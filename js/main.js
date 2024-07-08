746f52b0f7217d90999dad6002de190eb8106cd3
riku.sato <rkcc8809@gmail.com>
2024-07-09 06:12:39 +0900

    4th-commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,58 @@
 "use strict";
 
+// function EachTextAnimeControl() {
+//   $('.eachTextAnime').each(function () {
+//     var elemPos = $(this).offset().top - 50;
+//     var scroll = $(window).scrollTop();
+//     var windowHeight = $(window).height();
+//     if (scroll >= elemPos - windowHeight) {
+//       $(this).addClass("appeartext");
+//     } else {
+//       $(this).removeClass("appeartext");
+//     }
+//   });
+// }
+
+// $(window).on('load', function () {
+//     // spanタグを追加する
+//     var element = $(".eachTextAnime");
+//     element.each(function () {
+//       var text = $(this).html();
+//       var textbox = "";
+//       var delay = 0;
+
+//       text.split(/(<br\s*\/?>)/g).forEach(function (segment) {
+//         if (segment.match(/<br\s*\/?>/)) {
+//           // <br>タグはそのまま追加
+//           textbox += segment;
+//         } else {
+//           // テキスト部分を一文字ずつ分割
+//           segment.split('').forEach(function (t) {
+//             if (t !== " ") {
+//               textbox += '<span style="animation-delay:' + (delay * 0.05) + 's;">' + t + '</span>';
+//               delay++;
+//             } else {
+//               textbox += t;
+//             }
+//           });
+//         }
+//       });
+
+//       $(this).html(textbox);
+//     });
+
+//     EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+
+//     // 5秒後に .s-ttl に visible クラスを追加
+//     setTimeout(function () {
+//       $('.s-ttl').addClass('visible');
+//     }, 2200);
+// });
+
+// $(window).scroll(function () {
+//   EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+// });
+
 function EachTextAnimeControl() {
   $('.eachTextAnime').each(function () {
     var elemPos = $(this).offset().top - 50;
@@ -14,43 +67,47 @@ function EachTextAnimeControl() {
 }
 
 $(window).on('load', function () {
-    // spanタグを追加する
-    var element = $(".eachTextAnime");
-    element.each(function () {
-      var text = $(this).html();
-      var textbox = "";
-      var delay = 0;
+  // spanタグを追加する
+  var element = $(".eachTextAnime");
+  element.each(function () {
+    var text = $(this).html();
+    var textbox = "";
+    var delay = 0;
 
-      text.split(/(<br\s*\/?>)/g).forEach(function (segment) {
-        if (segment.match(/<br\s*\/?>/)) {
-          // <br>タグはそのまま追加
-          textbox += segment;
-        } else {
-          // テキスト部分を一文字ずつ分割
-          segment.split('').forEach(function (t) {
-            if (t !== " ") {
-              textbox += '<span style="animation-delay:' + (delay * 0.05) + 's;">' + t + '</span>';
-              delay++;
-            } else {
-              textbox += t;
-            }
-          });
-        }
-      });
-
-      $(this).html(textbox);
+    // <br>および<br class="__sp">タグを対象にする正規表現に変更
+    text.split(/(<br\s*\/?>|<br class="__sp"\s*\/?>)/g).forEach(function (segment) {
+      if (segment.match(/<br\s*\/?>|<br class="__sp"\s*\/?>/)) {
+        // <br>および<br class="__sp">タグはそのまま追加
+        textbox += segment;
+      } else {
+        // テキスト部分を一文字ずつ分割
+        segment.split('').forEach(function (t) {
+          if (t !== " ") {
+            textbox += '<span style="animation-delay:' + (delay * 0.05) + 's;">' + t + '</span>';
+            delay++;
+          } else {
+            textbox += t;
+          }
+        });
+      }
     });
 
-    EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+    $(this).html(textbox);
+  });
 
-    // 5秒後に .s-ttl に visible クラスを追加
-    setTimeout(function () {
-      $('.s-ttl').addClass('visible');
-    }, 2200);
+  // アニメーションを開始する直前に表示を設定
+  element.css('visibility', 'visible');
+
+  EachTextAnimeControl(); // アニメーション用の関数を呼ぶ
+
+  // 5秒後に .s-ttl に visible クラスを追加
+  setTimeout(function () {
+    $('.s-ttl').addClass('visible');
+  }, 2200);
 });
 
 $(window).scroll(function () {
-  EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+  EachTextAnimeControl(); // アニメーション用の関数を呼ぶ
 });
 
 
